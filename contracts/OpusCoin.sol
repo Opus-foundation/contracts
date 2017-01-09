054f35b15d5f65e0c69e0b6731168a61d56ca53f
chennyhaohao <chennyhaohao@gmail.com>
2017-01-09 11:54:38 +0400

    music licensing incorporated

--- contracts/OpusCoin.sol
+++ contracts/OpusCoin.sol
@@ -1,13 +1,99 @@
 pragma solidity ^0.4.2;
 
+import "./ConvertLib.sol";
 
-contract OpusCoin {
+
+contract OpusCoin{
+
 	mapping (address => uint) balances;
+  struct work{
+    uint id;
+    string name;
+    uint price;
+    address owner;
+    string resourceURL;
+  }
+
+	uint[] ids;
+
+  mapping (uint => work) private id2work;
+  //TODO: compare gas cost between using a mapping or an array
+  mapping (address => uint[]) private workOwnership;
+
+
+
+  function workExist(uint id) returns (bool){
+    //assuming id of an empty work is default 0
+    return (id2work[id].id == id);
+  }
+
+  function fetchURL(uint id) private returns (string){
+    if(!workExist(id)){
+      throw;
+    }
+    work _work = id2work[id];
+    return _work.resourceURL;
+  }
+
+  function requestURL(uint id) returns (string){
+    if(!ownWork(id)){
+			return "you do not own it";
+		}
+		return fetchURL(id);
+  }
+
+
+	function ownWork (uint id) returns (bool){
+		if(!workExist(id)){
+			return false;
+		}
+		uint[] works = workOwnership[msg.sender];
+    for(uint i=0; i<works.length; i++){
+      if (id == works[i]){
+        return true;
+      }
+	}
+	return false;
+	}
+
+	function getOwnedWorks() returns (uint[]){
+		return workOwnership[msg.sender];
+	}
+
+  function purchaseWork (uint id) {
+    if (!workExist(id)){
+			throw;
+      //return 1;
+    }
+		if (ownWork(id)){
+			throw;
+			//return 2;
+		}
+    work _work = id2work[id];
+    if(sendCoin(_work.owner, _work.price)){
+      workOwnership[msg.sender].push(id);
+			PurchaseWork(msg.sender, _work.id, _work.owner, _work.price);
+    }
+  }
+
+
+	function uploadWork(string _name, uint _price, string _resourceURL) {
+	//TODO: add log
+		lastId += 1;
+		work memory _work = work(lastId, _name, _price, msg.sender, _resourceURL);
+		id2work[lastId] = _work;
+		workOwnership[msg.sender].push(lastId);
+		ids.push(lastId);
+	}
+
+	function getLastId() returns (uint) {
+		return lastId;
+	}
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
+	event PurchaseWork(address purchaser, uint _id, address _owner, uint _price);
 	function OpusCoin() {
-		balances[msg.sender] = 10000;
+		balances[tx.origin] = 10000;
 	}
 
 	function transfer(address receiver, uint amount) returns(bool success) {
