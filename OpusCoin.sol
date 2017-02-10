ef0f8d698d53fee90164ff5da945689eaab0101b
chennyhaohao <chennyhaohao@gmail.com>
2017-02-10 11:24:31 +0400

    ERC23 token

--- OpusCoin.sol
+++ OpusCoin.sol
@@ -1,139 +1,18 @@
 
 pragma solidity ^0.4.8;
 
-import "./contracts/token/StandardToken.sol";
+import "../zeppelin-solidity/contracts/token/StandardToken.sol";
 import "./ConvertLib.sol";
 
 contract OpusCoin is StandardToken{
-//TODO: change id mechanism to hash of resourceURL
-//TODO: maybe seperate structs into independent contracts
-	uint private lastId = 0;
-	//mapping (address => uint) balances;
-  struct work{
-    uint id;
-    string name;
-    uint price;
-    address owner;
-    string resourceURL;
-		string artist;
-  }
-
-	uint[] ids;
-
-  mapping (uint => work) private id2work;
-  //TODO: compare gas cost between using a mapping or an array
-  mapping (address => uint[]) private workOwnership;
-
-	function getIds() returns (uint[]){
-		return ids;
-	}
-
-	function getWork(uint id) constant returns
-		(
-		uint _id,
-		string name,
-		string artist,
-		uint price
-		//address owner
-		){
-		if(!workExist(id)){
-			throw;
-		}
-		work _work = id2work[id];
-		_id = id;
-		name = _work.name;
-		price = _work.price;
-		//owner = _work.owner;
-		artist = _work.artist;
-	}
-
-  function workExist(uint id) constant returns (bool){
-    //assuming id of an empty work is default 0
-    return (id2work[id].id == id);
-  }
-
-  function fetchURL(uint id) private constant returns (string){
-    if(!workExist(id)){
-      throw;
-    }
-    work _work = id2work[id];
-    return _work.resourceURL;
-  }
-
-  function requestURL(uint id) constant returns (string){
-		//TODO: add log
-		//TODO: optimize checking cost; consider using mapping
-		//TODO: optimize for work owner
-		//Debug: when user does not own song
-    if(!ownWork(id)){
-			throw;
-			//return "you do not own it";
-		}
-		return fetchURL(id);
-  }
 
 
-	function ownWork (uint id) constant returns (bool){
-		if(!workExist(id)){
-			return false;
-		}
-		uint[] works = workOwnership[msg.sender];
-    for(uint i=0; i<works.length; i++){
-      if (id == works[i]){
-        return true;
-      }
-	}
-	return false;
-	}
-
-	function getOwnedWorks() constant returns (uint[]){
-		return workOwnership[msg.sender];
-	}
-
-  function purchaseWork (address placeholder, uint id) {
-	//redundant parameter to prevent short address attack
-    if (!workExist(id)){
-			throw;
-      //return 1;
-    }
-		if (ownWork(id)){
-			throw;
-			//return 2;
-		}
-    work _work = id2work[id];
-    workOwnership[msg.sender].push(id);
-		transfer(_work.owner, _work.price);
-		//PurchaseWork(msg.sender, _work.id, _work.owner, _work.price);
-  }
-
-
-	function uploadWork(string _name, uint _price, string _resourceURL, string _artist) {
-	//TODO: add log
-		lastId += 1;
-		work memory _work = work(lastId, _name, _price, msg.sender, _resourceURL, _artist);
-		id2work[lastId] = _work;
-		workOwnership[msg.sender].push(lastId);
-		ids.push(lastId);
-	}
-
-	function getLastId() returns (uint) {
-		return lastId;
-	}
-
-
-  //mapping private (address => uint) workOwnership;
-  //mapping private (uint => string) id2url;
-	//event Transfer(address indexed _from, address indexed _to, uint256 _value);
-	event PurchaseWork(address purchaser, uint _id, address _owner, uint _price);
 	function OpusCoin() {
 		balances[msg.sender] = 10000;
 	}
 
-	function getBalanceInEth(address addr) returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
 
-	function balanceOf(address addr) returns(uint) {
+	function getBalance(address addr) returns(uint) {
 		return balances[addr];
 	}
 
@@ -144,6 +23,47 @@ contract OpusCoin is StandardToken{
 	function getAddress() returns (address){
 		return msg.sender;
 	}
+	function transfer(address _to, uint _value) onlyPayloadSize(2 * 32){
+
+			//standard function transfer similar to ERC20 transfer with no _data
+			//added due to backwards compatibility reasons
+			if(blackList[_to]){
+				throw;
+			}
+
+			bytes memory empty;
+			if(isContract(_to)) {
+					transferToContract(_to, _value, empty);
+			}
+			else {
+					transferToAddress(_to, _value, empty);
+			}
+		}
+
+		function transferToAddress(address _to, uint _value, bytes _data) private {
+	    balances[msg.sender] = balances[msg.sender].sub(_value);
+	    balances[_to] = balances[_to].add(_value);
+	    Transfer(msg.sender, _to, _value);
+	  }
+
+	  /**
+	  * @dev Gets the balance of the specified address.
+	  * @param _owner The address to query the the balance of.
+	  * @return An uint representing the amount owned by the passed address.
+	  */
+	  function balanceOf(address _owner) constant returns (uint balance) {
+	    return balances[_owner];
+	 
+
+	  function transferToContract(address _to, uint _value, bytes _data) private returns(bool success){
+	    balances[msg.sender] = balances[msg.sender].sub( _value);
+	    balances[_to] = balances[_to].add( _value);
+	    Work reciever = Work(_to);
+	    reciever.tokenFallback(msg.sender, _value, _data);
+	    Transfer(msg.sender, _to, _value);
+	    //Transfer(msg.sender, _to, _value, _data);
+	    return true;
+	  }
 
 
 }
