c063879b61dfb726c9a50d63bb21034459944870
chennyhaohao <chennyhaohao@gmail.com>
2017-02-11 11:24:57 +0400

    token contract seperated from music licensing

---
+++ SongList.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.8;
+contract SongList{
+  address[] public songList;
+  mapping (address => address[]) public ownedSongs;
+  function addSong() external {
+    songList.push(msg.sender);
+  }
+  function getList() external constant returns(address[]){
+    return songList;
+  }
+  function ownSong(address guy) public {
+    ownedSongs[guy].push(msg.sender);
+  }
+  function getOwnedSongs() external constant returns(address[]){
+    return ownedSongs[msg.sender];
+  }
+}
