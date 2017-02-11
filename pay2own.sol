7491ca613430db996b9396102cad5cd0c8173c6a
chennyhaohao <chennyhaohao@gmail.com>
2017-02-11 16:18:56 +0400

    Minimize external function calls

--- pay2own.sol
+++ pay2own.sol
@@ -4,35 +4,23 @@ import './OpusCoin.sol';
 
 contract Work {
     using SafeMath for uint;
-    //enum WorkType {score, lyrics, recording}
-
-    //MusicoinLogger public logger;
     address owner;
     address private songListAddress;
     SongList songList;
     address private tokenAddress;
-    OpusCoin token;
-    //WorkType public workType;
-    string public title;
+    OpusCoin token;    string public title;
     string public artist;
-
-    // reference to a complete set of metadata
-    //string public metadataUrl;
-    //string public imageUrl;
+    string public metadataUrl;
+    string public imageUrl;
     string private audioUrl;
     uint public price;
     mapping (address => bool) private ownership;
 
     function Work(
-        //address _owner,
         address _token,
         address _songList,
-        //address _loggerAddress,
-        //WorkType _workType,
         string _title,
         string _artist,
-        //string _imageUrl,
-        //string _metadataUrl
         string _audioUrl,
         uint _price
         ) {
@@ -41,13 +29,8 @@ contract Work {
         token = OpusCoin(_token);
         songListAddress = _songList;
         songList = SongList(_songList);
-        //logger = MusicoinLogger(_loggerAddress);
-        //workType = _workType;
         title = _title;
         artist = _artist;
-        //metadataUrl = _metadataUrl;
-        //imageUrl = _imageUrl;
-        //logger.logWorkReleased(msg.sender, _title, _artist);
         audioUrl = _audioUrl;
         price = _price;
 
